8f7ab395b9f22efe29fe0e67b7bd8696f8e249db
ashwinhar <ashwin.hareesh@utexas.edu>
2023-11-14 09:48:41 -0500

    starting to structure query

--- ingest.py
+++ ingest.py
@@ -1,2 +1,15 @@
+import requests
+import os
+import json
 
-# TODO: Possible to automate updating the .yml file for the conda environment? 
+# A lot of this file is directly copied/refactored from the Recent-Search/recent_search.py file
+# from the Twtiter API v2 repo
+
+bearer_token = os.environ.get(
+    "AAAAAAAAAAAAAAAAAAAAAM7nqgEAAAAApYh%2FsB5PyLGQyK6RyrN50RpeNJA%3Dn2vR5o9gaLiPd4mOqQmeNe83vWMcnLggAQIhVRjaVGin9zPxJh")
+SEARCH_URL = "https://api.twitter.com/2/tweets/search/recent"
+
+query_params = {
+    'query': '#climatechange',
+    'tweet.fields': 'author_id'
+}
